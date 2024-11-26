79f4998dd000fed53c12965539d9caf52f3b0252
RainNeko <ningzhu549@gmail.com>
2024-11-25 21:47:49 -0500

    Add files via upload

--- Graph1D.java
+++ Graph1D.java
@@ -7,22 +7,22 @@ public class Graph1D {
     public int[] map;
     public List<List<Integer>> list;
 
-    /** default constructor */
+    // default constructor
     public Graph1D () {}
 
-    /** constructor */
+    // constructor
     public Graph1D (int capacity) {
-        // base length: the number of vertices of the first row in the initial 2D graph
+        // base length is the number of vertices of the first row in the initial 2D graph
         base_len = capacity - 1;
 
         // the formula to calculate the length of the 1D array converted
         int arr_len = (base_len * (base_len + 1)) / 2;
 
-        // initialize the 1D array (map) and fill by Integer.MAX_VALUE
+        // initialize the map and fill by Integer.MAX_VALUE, represents no connection between vertices
         map = new int[arr_len];
         Arrays.fill(map, Integer.MAX_VALUE);
 
-        // initialize the array list to sort the horizontal and vertical coordinates of every vertex, initially are null
+        // initialize the array list used to sort the row and column coordinates of every vertex
         list = new ArrayList<>(arr_len);
         for (int i = 0; i < arr_len; i++) list.add(new ArrayList<>(Arrays.asList(null, null)));
     }
@@ -36,7 +36,7 @@ public class Graph1D {
     /** print the list of the graph */
     public void displayList () { System.out.printf ("list: %s\n", list); }
 
-    /** add two vertices with a weighted edge */
+    /** add two vertices */
     public void addEdge (int vertex1, int vertex2, int weight) {
         if (vertex2 <= vertex1) throw new IllegalArgumentException("illegal parameter sequence");
         int index = (vertex1 == 0) 
@@ -62,20 +62,25 @@ public class Graph1D {
         list.get(index).set(1, null);
     }
 
-    /** get the sum of minimum spanning tree (MST) */
+    /** get the sum of MST */
     public int getMSTValue () {
+        // convert the map to priority queue
         Queue<Integer> queue = Arrays.stream(map)
                                      .boxed()
                                      .collect(PriorityQueue::new, PriorityQueue::add, PriorityQueue::addAll);
+        // initialize a hash set to store cordinates to identify if a vertex is addable in the MST
         Set<Integer> set = new HashSet<>();
         int sum = 0, count = 0, size = queue.size();
         for (int i = 0; i < size; i++) {
+            // identify the addability of each edge
             if (list.get(i).get(0) != null && !(set.contains(list.get(i).get(0)) && set.contains(list.get(i).get(1)))) {
                     sum += queue.poll();
+                    // store the coordinate in the hash set for further judgements
                     set.add(list.get(i).get(0));
                     set.add(list.get(i).get(1));
                     count++;
             } else queue.poll();
+            // break the loop when m = n - 1 (m = base_len)
             if (count == base_len) break;
         }
         return sum;
@@ -83,7 +88,7 @@ public class Graph1D {
 
     /** test used main function */
     public static void main(String[] args) {
-        // example instance Graph1D for test
+        // instance Graph1D for test
         Graph1D graph = new Graph1D(4);
 
         // add edges
@@ -97,7 +102,7 @@ public class Graph1D {
         // print the base length
         System.out.printf ("%s%d\n", "base length: ", graph.getBaseLength());
 
-        // print the minimum spanning tree (MST) value
+        // print the MST value
         System.out.printf ("%s%d\n", "MST value: ", graph.getMSTValue());
 
         // print the list
